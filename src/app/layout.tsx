6b05a6037e4ba734e79f1d882b356f27f5af758a
Gaëtan H <steellgold0@gmail.com>
2023-08-05 13:44:12 +0200

    chore(package.json): add "@vercel/analytics" package feat(layout.tsx): add <Analytics /> component from "@vercel/analytics/react" package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import { HomeLayoutGradient } from "@/lib/components/layout/gradient";
 import { ThemeProvider } from "@/lib/components/theme-provider";
 import type { Component } from "@/lib/utils/component";
+import { Analytics } from "@vercel/analytics/react";
 export { metadata } from "@/lib/configs/metadata";
 import { Navbar } from "@/lib/molecules/navbar";
 import type { PropsWithChildren } from "react";
@@ -16,6 +17,8 @@ const RootLayout: Component<PropsWithChildren> = ({ children }) => {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={cn("bg-white dark:bg-[#09090b]", os.className)}>
+        <Analytics />
+
         <ThemeProvider attribute="class" defaultTheme="dark">
           <HomeLayoutGradient />
 
