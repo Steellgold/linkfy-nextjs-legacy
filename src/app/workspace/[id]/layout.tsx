2aed89d841dbab7bd257c2035a7c3ad21c4ece58
Gaëtan H <steellgold0@gmail.com>
2023-08-12 17:45:09 +0200

    chore(layout.tsx): clean up imports and add Link component from next/link feat(layout.tsx): add return to Workspaces button chore(page.tsx): clean up imports and add Link component from next/link feat(page.tsx): add content and buttons for managing workspaces

--- src/app/workspace/[id]/layout.tsx
+++ src/app/workspace/[id]/layout.tsx
@@ -1,5 +1,7 @@
+import { buttonVariants } from "@/lib/components/ui/button"
 import { Separator } from "@/lib/components/ui/separator"
 import { SidebarNav } from "@/lib/molecules/sidebar"
+import Link from "next/link"
 
 const sidebarNavItems = [
   { title: "Overview", href: null },
@@ -17,22 +19,24 @@ interface WorkspaceLayoutProps {
 
 export default function WorkspaceLayout({ children }: WorkspaceLayoutProps) {
   return (
-    <>
-      <div className="space-y-6 p-10 pb-16 md:block">
-        <div className="space-y-0.5">
-          <h2 className="text-2xl font-bold tracking-tight">Workspace</h2>
-          <p className="text-muted-foreground">
-            Manage your workspace settings and set up billing here.
-          </p>
+    <div className="mx-auto mt-14 max-w-screen-xl">
+      <div className="space-y-6 p-2 pb-16 md:block">
+        <div className="space-y-0.5 flex justify-between">
+          <div className="flex flex-col">
+            <h2 className="text-2xl font-bold tracking-tight">Workspace</h2>
+            <p className="text-muted-foreground">
+              Manage your workspace settings and set up billing here.
+            </p>
+          </div>
+          <Link href={"/workspaces"} className={buttonVariants({ variant: "outline", size: "sm" })}>
+            Return to Workspaces
+          </Link>
         </div>
         <Separator className="my-6" />
         <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-          <aside className="-mx-4 lg:w-1/5">
-            <SidebarNav items={sidebarNavItems} />
-          </aside>
           <div className="flex-1 lg:max-w-5xl">{children}</div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
--- src/app/workspaces/page.tsx
+++ src/app/workspaces/page.tsx
@@ -1,9 +1,41 @@
 "use client";
 
+import { Button, buttonVariants } from "@/lib/components/ui/button";
+import { Separator } from "@/lib/components/ui/separator";
+import Link from "next/link";
+
 const Home = (): React.ReactElement => {
   return (
-    <>
-    </>
+    <div className="mx-auto mt-14 max-w-screen-xl">
+      <div className="space-y-6 p-2 pb-16 md:block">
+        <div className="space-y-0.5 flex justify-between">
+          <div className="flex flex-col">
+            <h2 className="text-2xl font-bold tracking-tight">Workspaces</h2>
+            <p className="text-muted-foreground">
+              Manage your workspaces, invite people, create projects and more!
+            </p>
+          </div>
+        </div>
+        <Separator className="my-6" />
+        {/* div au millieu de la page (en largeur), avec mt-16 et des bordure fine pas trop voyante, avec un padding de 10 */}
+        <div className="flex flex-col items-center justify-center space-y-6 mt-16 border border-gray-100/10 rounded-md p-10">
+          <div className="flex flex-col space-y-2">
+            <h3 className="text-lg font-bold tracking-tight">No workspaces</h3>
+            <p className="text-muted-foreground">
+              You don&apos;t have any workspaces yet, create one or be invited!
+            </p>
+            <div className="flex space-x-2">
+              <Button variant={"outline"} className="w-full" disabled>
+                Create a workspace
+              </Button>
+              <Link href={"/workspace/personal"} className={buttonVariants({ variant: "outline", size: "sm", className: "w-full" })}>
+                Go to personal workspace
+              </Link>
+            </div>
+            </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
