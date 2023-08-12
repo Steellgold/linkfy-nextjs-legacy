48ecc2906b3ac9583ae71ecaad02df4759261001
Gaëtan H <steellgold0@gmail.com>
2023-08-12 18:03:43 +0200

    chore(schema.prisma): add enum Role with ADMIN, TESTER, and USER values feat(page.tsx): add support for displaying workspaces and creating new workspaces fix(layout.tsx): add missing SidebarNav component to display sidebar navigation

--- src/app/workspace/[id]/layout.tsx
+++ src/app/workspace/[id]/layout.tsx
@@ -34,6 +34,9 @@ export default function WorkspaceLayout({ children }: WorkspaceLayoutProps) {
         </div>
         <Separator className="my-6" />
         <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+          <aside className="-mx-4 lg:w-1/5">
+            <SidebarNav items={sidebarNavItems} />
+          </aside>
           <div className="flex-1 lg:max-w-5xl">{children}</div>
         </div>
       </div>
